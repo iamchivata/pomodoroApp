f2f178f83ccad9af165ca097e41d16386f5e73f9
chivaata <Chivatavakol@gmail.com>
2020-05-09 15:37:47 +0430

    the current date added to top of the page

--- pomodoro.js
+++ pomodoro.js
@@ -2,44 +2,107 @@ window.onload = function(){
 
   var countdown = 25*60*1000;
 
+  
+  var today= new Date();
+  var month =today.getMonth()+1;
 
-    document.getElementById("start").onclick= function(){
+  document.getElementById("year").innerHTML= today.getFullYear();
+  
+   switch(month){
+    case 1:
+      document.getElementById("month").innerHTML="Jan";
+      break;
+
+    case 2:
+      document.getElementById("month").innerHTML="Feb";
+      break;
+
+    case 3:
+      document.getElementById("month").innerHTML="March";
+      break;
+
+    case 4:
+      document.getElementById("month").innerHTML="April";
+      break;
+
+    case 5:
+      document.getElementById("month").innerHTML="May";
+      break;
+
+    case 6:
+      document.getElementById("month").innerHTML="Jun";
+      break;
+    
+    case 7:
+      document.getElementById("month").innerHTML="July"
+      break;
+     
+    case 8:
+      document.getElementById("month").innerHTML="Aug";
+      break;
+
+    case 9:
+      document.getElementById("month").innerHTML="Sep";
+      break;
+      
+    case 10:
+      document.getElementById("month").innerHTML="Oct";
+      break;
+      
+    case 11:
+      document.getElementById("month").innerHTML="Nov";
+      break;
+      
+    case 12:
+      document.getElementById("month").innerHTML="Dec";
+      break;
+   }
+
+  
+
+  // function for start timer:
+  document.getElementById("start").onclick= function(){
       window.timerId = setInterval(function(){
      
        countdown -= 1000;
      
        var min = Math.floor(countdown / (60 * 1000));
        var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);  //correct
-     
+       
+       if (min<10){
+         min= "0"+min;
+       }
+
+       if (sec<10){
+         sec= "0"+sec;
+       }
+
        if (countdown < 0) {
         clearInterval(timerId);
         var audio = new Audio('ding.mp3');
         audio.play();
-      } 
+       } 
 
-      else {
+       else {
        document.getElementById("timer").innerHTML =min + " : " + sec;
-     }
+       }
      
-       }, 1000);
+      }, 1000);
   };
 
 
  document.getElementById("pause").onclick=function(){
   console.log("pause");
-  // clearInterval(timerId);
-  // alert("pause");
   clearInterval(window.timerId);
-}
+ }
  
 
-
-document.getElementById("reset").onclick= function(){
+ document.getElementById("reset").onclick= function(){
   console.log("reset");
   clearInterval(window.timerId);
   countdown=25*60*1000;
   document.getElementById("timer").innerHTML= "25:00";
-};
+ };
 
 
 }
