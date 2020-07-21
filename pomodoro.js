897c93bcfa65dbb78b8f6d5303f091cc4cab2915
negar mohamadhassan <nemote95@gmail.com>
2020-07-21 16:11:47 +1200

    added breaks and pomodors counts
    # need to disable the start button when it's counting down

--- pomodoro.js
+++ pomodoro.js
@@ -1,67 +1,70 @@
 window.onload = function(){
+  var pomodoro_length = 1
+  var countdown = pomodoro_length *60*1000;
+  var num_promodos =0;
+  var break_length = 0.5;
 
-  var countdown = 25*60*1000;
-
+  document.getElementById("timer").innerHTML= pomodoro_length +":00";
 
   var today= new Date();
-  var month =today.getMonth()+1;
-
-  document.getElementById("year").innerHTML= today.getFullYear();
-  
-   switch(month){
+  var month_num = today.getMonth();
+   switch(month_num){
     case 1:
-      document.getElementById("month").innerHTML="Jan";
+      month="Jan";
       break;
 
     case 2:
-      document.getElementById("month").innerHTML="Feb";
+      month="Feb";
       break;
 
     case 3:
-      document.getElementById("month").innerHTML="March";
+      month="March";
       break;
 
     case 4:
-      document.getElementById("month").innerHTML="April";
+      month="April";
       break;
 
     case 5:
-      document.getElementById("month").innerHTML="May";
+      month="May";
       break;
 
     case 6:
-      document.getElementById("month").innerHTML="Jun";
+      month="Jun";
       break;
     
     case 7:
-      document.getElementById("month").innerHTML="July"
+      month="July"
       break;
      
     case 8:
-      document.getElementById("month").innerHTML="Aug";
+      month="Aug";
       break;
 
     case 9:
-      document.getElementById("month").innerHTML="Sep";
+      month="Sep";
       break;
       
     case 10:
-      document.getElementById("month").innerHTML="Oct";
+      month="Oct";
       break;
       
     case 11:
-      document.getElementById("month").innerHTML="Nov";
+      month="Nov";
       break;
       
     case 12:
-      document.getElementById("month").innerHTML="Dec";
+      month="Dec";
       break;
    }
 
-  
+    document.getElementById("date").innerHTML= today.getDate()+" "+month+" "+today.getFullYear();
+    document.getElementById("promodos").innerHTML ="Today's pomodoros: "+num_promodos ;
 
   // function for start timer:
   document.getElementById("start").onclick= function(){
+      document.getElementById("message").innerHTML ="";
+      countdown=pomodoro_length *60*1000;
       window.timerId = setInterval(function(){
      
        countdown -= 1000;
@@ -80,11 +83,15 @@ window.onload = function(){
        if (countdown < 0) {
         clearInterval(timerId);
         var audio = new Audio('ding.mp3');
+	num_promodos++;
+	document.getElementById("promodos").innerHTML ="Today's pomodoros: "+num_promodos ;
         audio.play();
+	pomodoro_break();
        } 
 
        else {
        document.getElementById("timer").innerHTML =min + " : " + sec;
+
        }
      
       }, 1000);
@@ -100,10 +107,45 @@ window.onload = function(){
  document.getElementById("reset").onclick= function(){
   console.log("reset");
   clearInterval(window.timerId);
-  countdown=25*60*1000;
-  document.getElementById("timer").innerHTML= "25:00";
+  countdown=pomodoro_length *60*1000;
+  break_countdown=0;
+  document.getElementById("timer").innerHTML= pomodoro_length +":00";
  };
+//break function:
+ function pomodoro_break(){
+      console.log("time for a break");
+      document.getElementById("message").innerHTML ="Enjoy your break :)";
+      document.getElementById("timer").innerHTML = break_length+":00"
+      break_countdown=break_length*60*1000;
+      window.timerId = setInterval(function(){
+     
+       break_countdown -= 1000;
+     
+       var min = Math.floor(break_countdown / (60 * 1000));
+       var sec = Math.floor((break_countdown - (min * 60 * 1000)) / 1000);  //correct
+       
+       if (min<10){
+         min= "0"+min;
+       }
 
+       if (sec<10){
+         sec= "0"+sec;
+       }
+
+       if (break_countdown < 0) {
+        clearInterval(timerId);
+        var audio = new Audio('ding.mp3');
+        audio.play();
+	document.getElementById("message").innerHTML ="Let's get back to work!";
+	document.getElementById("timer").innerHTML =pomodoro_length +":00";
+       } 
+
+       else {
+       document.getElementById("timer").innerHTML =min + " : " + sec;
+       }
+     
+      }, 1000);
+  };
 
 }
 
