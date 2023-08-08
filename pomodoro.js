4ebc74e2694df3504380203378bcb22f701d583d
Studio Primer <arbazargani1998@gmail.com>
2023-08-08 19:04:02 +0330

    Removed a boring switchCase :)

--- pomodoro.js
+++ pomodoro.js
@@ -1,114 +1,77 @@
-window.onload = function(){
+function MapMonthNumber(month) {
+    var dataBag = [
+        'Jan',
+        'Feb',
+        'March',
+        'April',
+        'May',
+        'Jun',
+        'July',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ];
 
-  var countdown = 25*60*1000;
+    return (dataBag[month - 1] == undefined) ? false : dataBag[month - 1];
+}
+
+window.onload = function() {
+
+    var countdown = 25 * 60 * 1000;
 
 
-  var today= new Date();
-  var month =today.getMonth()+1;
+    var today = new Date();
+    var month = today.getMonth() + 1;
 
-  document.getElementById("year").innerHTML= today.getFullYear();
-  
-   switch(month){
-    case 1:
-      document.getElementById("month").innerHTML="Jan";
-      break;
+    document.getElementById("year").innerHTML = today.getFullYear();
 
-    case 2:
-      document.getElementById("month").innerHTML="Feb";
-      break;
+    month = MapMonthNumber(month);
 
-    case 3:
-      document.getElementById("month").innerHTML="March";
-      break;
 
-    case 4:
-      document.getElementById("month").innerHTML="April";
-      break;
 
-    case 5:
-      document.getElementById("month").innerHTML="May";
-      break;
+    // function for start timer:
+    document.getElementById("start").onclick = function() {
+        window.timerId = setInterval(function() {
 
-    case 6:
-      document.getElementById("month").innerHTML="Jun";
-      break;
-    
-    case 7:
-      document.getElementById("month").innerHTML="July"
-      break;
-     
-    case 8:
-      document.getElementById("month").innerHTML="Aug";
-      break;
+            countdown -= 1000;
 
-    case 9:
-      document.getElementById("month").innerHTML="Sep";
-      break;
-      
-    case 10:
-      document.getElementById("month").innerHTML="Oct";
-      break;
-      
-    case 11:
-      document.getElementById("month").innerHTML="Nov";
-      break;
-      
-    case 12:
-      document.getElementById("month").innerHTML="Dec";
-      break;
-   }
+            var min = Math.floor(countdown / (60 * 1000));
+            var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000); //correct
 
-  
+            if (min < 10) {
+                min = "0" + min;
+            }
 
-  // function for start timer:
-  document.getElementById("start").onclick= function(){
-      window.timerId = setInterval(function(){
-     
-       countdown -= 1000;
-     
-       var min = Math.floor(countdown / (60 * 1000));
-       var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);  //correct
-       
-       if (min<10){
-         min= "0"+min;
-       }
+            if (sec < 10) {
+                sec = "0" + sec;
+            }
 
-       if (sec<10){
-         sec= "0"+sec;
-       }
+            if (countdown < 0) {
+                clearInterval(timerId);
+                var audio = new Audio('ding.mp3');
+                audio.play();
+            } else {
+                document.getElementById("timer").innerHTML = min + " : " + sec;
+            }
 
-       if (countdown < 0) {
-        clearInterval(timerId);
-        var audio = new Audio('ding.mp3');
-        audio.play();
-       } 
+        }, 1000);
+    };
 
-       else {
-       document.getElementById("timer").innerHTML =min + " : " + sec;
-       }
-     
-      }, 1000);
-  };
+    // Pause function:
+    document.getElementById("pause").onclick = function() {
+        console.log("pause");
+        clearInterval(window.timerId);
+    }
 
-// Pause function:
- document.getElementById("pause").onclick=function(){
-  console.log("pause");
-  clearInterval(window.timerId);
- }
- 
-// Reset function:
- document.getElementById("reset").onclick= function(){
-  console.log("reset");
-  clearInterval(window.timerId);
-  countdown=25*60*1000;
-  document.getElementById("timer").innerHTML= "25:00";
- };
+    // Reset function:
+    document.getElementById("reset").onclick = function() {
+        console.log("reset");
+        clearInterval(window.timerId);
+        countdown = 25 * 60 * 1000;
+        document.getElementById("timer").innerHTML = "25:00";
+    };
 
 
 }
-
-
-
-
- 
-
